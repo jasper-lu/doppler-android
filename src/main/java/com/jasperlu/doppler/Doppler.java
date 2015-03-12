3fff34b68d17b617426abb0c4a44c1ad41d02ab7
jasperlu <repsajrules@gmail.com>
2015-03-12 15:09:29 -0400

    doppler sound plays the frequency now. Going to split it off into it's own class

--- src/main/java/com/jasperlu/doppler/Doppler.java
+++ src/main/java/com/jasperlu/doppler/Doppler.java
@@ -17,7 +17,7 @@ import android.util.Xml;
  * http://stackoverflow.com/questions/18652000/record-audio-in-java-and-determine-real-time-if-a-tone-of-x-frequency-was-played
  */
 public class Doppler {
-    public static final int PRELIM_FREQ = 723;
+    public static final int PRELIM_FREQ = 20000;
     public static final int DEFAULT_SAMPLE_RATE = 44100;
     //in milliseconds
     public static final int INTERVAL = 3000;
@@ -31,31 +31,41 @@ public class Doppler {
 
     public Doppler() {
         //write a check to see if stereo is supported
-        Integer bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_16BIT);
+        Integer bufferSize = getNumSamples() * 2;
         microphone = new AudioRecord(MediaRecorder.AudioSource.MIC, DEFAULT_SAMPLE_RATE,
                 AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
         audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_OUT_STEREO,
                 AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STATIC);
-        generatedSound = new byte[getNumSamples() * 2];
 
-        generateTone(generatedSound, SAMPLE_RATE, getNumSamples(), PRELIM_FREQ);
+        generatedSound = generateTone(SAMPLE_RATE, getNumSamples(), PRELIM_FREQ);
         audioTrack.write(generatedSound, 0, generatedSound.length);
     }
 
     public boolean start() {
         try {
             //microphone.startRecording();
+            /*
             final Handler handler = new Handler();
             final Runnable runnable = new Runnable() {
                 @Override
                 public void run() {
-                    //Log.d("DOPPLER", "Testing");
-
+                    Log.d("DOPPLER", "Testing");
+                    audioTrack.stop();
+                    Log.d("DOPPLER", "stopped");
+                    audioTrack.reloadStaticData();
+                    Log.d("DOPPLER", "reload");
                     audioTrack.play();
+                    Log.d("DOPPLER", "playing");
                     handler.postDelayed(this, INTERVAL);
                 }
             };
             runnable.run();
+            */
+
+            //frame size is 4 because i have stereo on as well. frame size = length / (stereo vs mono * 16 vs 8 pcm)
+            Log.d("loop points", audioTrack.setLoopPoints(0, generatedSound.length /4, -1) + "");
+
+            audioTrack.play();
             return true;
         } catch (Exception e) {
             e.printStackTrace();
@@ -67,7 +77,7 @@ public class Doppler {
         return SAMPLE_RATE * INTERVAL / MILLI_PER_SECOND;
     }
 
-    private void generateTone(byte[] track, int sampleRate, int numSamples, int frequency) {
+    private byte[] generateTone(int sampleRate, int numSamples, int frequency) {
         Log.d("Generate tone", "Sample Rate: " + sampleRate + " numSamples " + numSamples);
         // fill out the array
         double[] sample = new double[numSamples];
@@ -77,6 +87,7 @@ public class Doppler {
 
         // convert to 16 bit pcm sound array
         // assumes the sample buffer is normalised.
+        byte[] track = new byte[getNumSamples() * 2];
         int idx = 0;
         for (final double dVal : sample) {
             // scale to maximum amplitude
@@ -85,6 +96,7 @@ public class Doppler {
             track[idx++] = (byte) (val & 0x00ff);
             track[idx++] = (byte) ((val & 0xff00) >>> 8);
         }
+        return track;
     }
 
     private void test() {
